769b03c988e98703212948e65bbad3cf73b1aa38
abordachan <andrea.bordachan@gmail.com>
2026-04-24 23:21:56 -0400

    donate buttons

---
+++ donate_button.js
@@ -0,0 +1,12 @@
+// Script for the donate buttons on the conservation page
+document.getElementById("cascadia")?.addEventListener("click", () => {
+    window.open("https://cascadiaresearch.org/support_crc/", "_blank");
+});
+
+document.getElementById("nmfs")?.addEventListener("click", () => {
+    window.open("https://marinesanctuary.org/get-involved/ways-to-give/", "_blank");
+});
+
+document.getElementById("hwf")?.addEventListener("click", () => {
+    window.open("https://www.wildhawaii.org/donate/", "_blank");
+});
